47f30bc6561e113ecdca6bb82fdff4f2d7b7d85a
Naght <benjazamorano@gmail.com>
2026-02-27 23:18:32 -0300

    Cambio en algunos archivos

--- src/main/java/com/duoc/hotel_api/HotelApiApplication.java
+++ src/main/java/com/duoc/hotel_api/HotelApiApplication.java
@@ -1,10 +1,17 @@
-package com.duoc.hotel_api;
+package com.duoc.hotelapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HotelApiApplication {
+public class HotelApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HotelApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApiApplication.class, args);
